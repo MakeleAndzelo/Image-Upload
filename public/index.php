bf1f76b171295506562fe93cf2ef7d14447cb3f5
MakeleAndzelo <97.piotrowski.michal@gmail.com>
2017-03-02 18:58:22 +0100

    Cleaned up the code

--- public/index.php
+++ public/index.php
@@ -1,29 +1,10 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
+require __DIR__ . "/../bootstrap/autoload.php";
 
-require __DIR__ . "/../vendor/autoload.php";
+require __DIR__ . '/../bootstrap/app.php';
 
-$app = new Silex\Application;
-
-$app->register(new Silex\Provider\TwigServiceProvider, [
-	'twig.path' => __DIR__.'/../views'
-]);
-
-$app->register(new Silex\Provider\DoctrineServiceProvider, [
-	'db.options' => [
-		'driver' => 'pdo_mysql',
-		'dbname' => 'image_upload',
-		'user' => 'root',
-		'password' => 'kdRPyFMp',
-		'host' => 'localhost',
-	]
-]);
-
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider);
-
-$app->register(new IU\Providers\UploadcareProvider);
-
+// Mounted images controller
 $app->mount('/', new \IU\Controllers\ImagesController);
 
 $app->run();
